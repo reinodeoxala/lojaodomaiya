8e6d280c552a19b5aa39592d1a0baaf285683062
reinodeoxala <wiliamrenan55@gmail.com>
2026-02-05 09:33:12 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,2 @@
-import { db } from "./firebase.js";
-import {
-  collection,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const produtosContainer = document.getElementById("produtos");
-
-async function carregarProdutos() {
-  const querySnapshot = await getDocs(collection(db, "produtos"));
-  produtosContainer.innerHTML = "";
-
-  querySnapshot.forEach((doc) => {
-    const p = doc.data();
-    produtosContainer.innerHTML += `
-      <div class="card">
-        <img src="${p.imagem}" loading="lazy">
-        <h3>${p.nome}</h3>
-        <p>R$ ${p.preco}</p>
-        <span>Estoque: ${p.estoque}</span>
-      </div>
-    `;
-  });
-}
-
-carregarProdutos();
+document.getElementById("produtos").innerHTML =
+  "<h2 style='text-align:center'>JS FUNCIONANDO ✔</h2>"
